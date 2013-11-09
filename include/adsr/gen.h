ae47981f49a255919897f44f82d08270531836b7
Stephen Demos <stphndemos@gmail.com>
2013-11-09 15:01:42 -0500

    yo watup, fixed the function pointer

--- include/adsr/gen.h
+++ include/adsr/gen.h
@@ -6,18 +6,18 @@
 typedef struct
 {
 	mpfr_t freq;
-	mpfr_t (*gen_handler)(*adsr_gen_t);
+	mpfr_t (*gen_handler)(adsr_gen_t *gen);
 	void *opt;
 	void *mod_next;
 	gen_t *gen_next;
 } adsr_gen_t;
 
-mpfr_t adsr_gen_square(adsr_gen_t gen);
-mpfr_t adsr_gen_triangle(adsr_gen_t gen);
-mpfr_t adsr_gen_sawtooth(adsr_gen_t gen);
-mpfr_t adsr_gen_sine(adsr_gen_t gen);
-mpfr_t adsr_gen_noise(adsr_gen_t gen);
-mpfr_t adsr_gen_custom_register(adsr_gen_t gen);
-mpfr_t adsr_gen_custom_function(adsr_gen_t gen);
+mpfr_t adsr_gen_square(adsr_gen_t *gen);
+mpfr_t adsr_gen_triangle(adsr_gen_t *gen);
+mpfr_t adsr_gen_sawtooth(adsr_gen_t *gen);
+mpfr_t adsr_gen_sine(adsr_gen_t *gen);
+mpfr_t adsr_gen_noise(adsr_gen_t *gen);
+mpfr_t adsr_gen_custom_register(adsr_gen_t *gen);
+mpfr_t adsr_gen_custom_function(adsr_gen_t *gen);
 
 #endif
